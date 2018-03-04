4a95673ef8e8c747c13b23ef70cfa575e0c4896f
VTariko <vv.tariko@gmail.com>
2018-03-04 13:56:45 +0300

    Контрол переписан как самостоятелньый элемент, который можно вставить куда угодно

--- WpfMailSender/BarComboBox/ToolBarComboButton.xaml.cs
+++ WpfMailSender/BarComboBox/ToolBarComboButton.xaml.cs
@@ -18,11 +18,81 @@ namespace BarComboBox
     /// <summary>
     /// Interaction logic for ToolBarControlButton.xaml
     /// </summary>
-    public partial class ToolBarControlButton : TabControl
+    public partial class ToolBarControlButton : UserControl
     {
+        #region События
+
+        /// <summary>
+        /// Обработка нажатия клавиши Добавить
+        /// </summary>
+        public event RoutedEventHandler BtnAddClick;
+
+        /// <summary>
+        /// Обработка нажатия клавиши Редактировать
+        /// </summary>
+        public event RoutedEventHandler BtnEditClick;
+
+        /// <summary>
+        /// Обработка нажатия клавиши Удалить
+        /// </summary>
+        public event RoutedEventHandler BtnDelClick;
+
+        #endregion
+
+        #region Свойства
+
+        public string LabelText
+        {
+            get => (string) lblHeader.Content;
+            set => lblHeader.Content = value;
+        }
+
+        public Dictionary<string, object> ComboBoxCollection { get; set; }
+
+
+        #endregion
+
+        #region Конструктор
+
         public ToolBarControlButton()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Обработка нажатия кнопки Добавить
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            BtnAddClick?.Invoke(sender, e);
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки Редактировать
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnEdit_OnClick(object sender, RoutedEventArgs e)
+        {
+            BtnEditClick?.Invoke(sender, e);
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки Удалить
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDel_OnClick(object sender, RoutedEventArgs e)
+        {
+            BtnDelClick?.Invoke(sender, e);
+        }
+
+        #endregion
     }
 }
